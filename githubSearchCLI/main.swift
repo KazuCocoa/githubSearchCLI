6256c328feeb3a70e2c4cb6a1582e9ff45875af2
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-02-11 17:31:49 +0900

    Initial Commit

---
+++ githubSearchCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  githubSearchCLI
+//
+//  Created by kazuaki matsuo on 2017/02/11.
+//  Copyright © 2017 kazuaki matsuo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
