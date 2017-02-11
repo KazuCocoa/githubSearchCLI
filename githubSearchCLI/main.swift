09aaee5fac19cce10e225f1e8a826dd543b08b81
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-02-11 19:18:53 +0900

    implement command line

--- githubSearchCLI/main.swift
+++ githubSearchCLI/main.swift
@@ -1,12 +1,30 @@
-//
-//  main.swift
-//  githubSearchCLI
-//
-//  Created by kazuaki matsuo on 2017/02/11.
-//  Copyright © 2017 kazuaki matsuo. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+print("Enter your query here>", terminator: "")
 
+guard let keyword = readLine(strippingNewline: true) else {
+    exit(1)
+}
+
+let client = GitHubClient()
+
+let request = GitHubAPI.SearchRepositries(keyword: keyword)
+
+client.send(request: request) { result in
+    switch result {
+    case let .success(response):
+        for item in response.items {
+            print(item.owner.login + "/" + item.name)
+        }
+        exit(0)
+    case let .failure(error):
+        print(error)
+        exit(1)
+    }
+}
+
+let timeoutInterval: TimeInterval = 60
+Thread.sleep(forTimeInterval: timeoutInterval)
+
+print("connection timeout")
+exit(1)
