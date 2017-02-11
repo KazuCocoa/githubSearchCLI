e2e1b2c158f4afb73ce81418726b9cf84fb67250
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-02-11 19:11:41 +0900

    add client

---
+++ githubSearchCLI/GitHubAPI.swift
@@ -0,0 +1,37 @@
+final class GitHubAPI {
+    struct SearchRepositries : GitHubRequest {
+        let keyword: String
+
+        typealias Response = SearchResponse<Repository>
+
+        var method: HTTPMethod {
+            return .get
+        }
+
+        var path: String {
+            return "/search/repositories"
+        }
+
+        var parameters: Any? {
+            return ["q": keyword]
+        }
+    }
+
+    struct SearchUsers : GitHubRequest {
+        let keyword: String
+
+        typealias Response = SearchResponse<User>
+
+        var method: HTTPMethod {
+            return .get
+        }
+
+        var path: String {
+            return "/search/users"
+        }
+
+        var parameters: Any? {
+            return ["q": keyword]
+        }
+    }
+}
--- githubSearchCLI/GitHubAPIError.swift
+++ githubSearchCLI/GitHubAPIError.swift
@@ -1,5 +1,6 @@
 struct GitHubAPIError : JSONDecodable, Error {
     let message: String
+    let fieldErrors: [FieldError]
     
     init(json: Any) throws {
         guard let dictionary = json as? [String : Any] else {
@@ -10,7 +11,44 @@ struct GitHubAPIError : JSONDecodable, Error {
             throw JSONDecodeError.missingValue(key: "message",
                                                actualValue: dictionary["message"])
         }
+
+        let fieldErrorObjects = dictionary["errors"] as? [Any] ?? []
+        let fieldErrors = try fieldErrorObjects.map {
+            return try FieldError(json: $0)
+        }
         
         self.message = message
+        self.fieldErrors = fieldErrors
+    }
+    
+    struct FieldError : JSONDecodable {
+        let resource: String
+        let field: String
+        let code: String
+        
+        init(json: Any) throws {
+            guard let dictionary = json as? [String : Any] else {
+                throw JSONDecodeError.invalidFormat(json: json)
+            }
+            
+            guard let resource = dictionary["resource"] as? String else {
+                throw JSONDecodeError.missingValue(key: "resource",
+                                                   actualValue: dictionary["resource"])
+            }
+            
+            guard let field = dictionary["field"] as? String else {
+                throw JSONDecodeError.missingValue(key: "field",
+                                                   actualValue: dictionary["field"])
+            }
+            
+            guard let code = dictionary["code"] as? String else {
+                throw JSONDecodeError.missingValue(key: "code",
+                                                   actualValue: dictionary["code"])
+            }
+
+            self.resource = resource
+            self.field = field
+            self.code = code
+        }
     }
 }
