b261255c3620a44b7cf546831788cfa8d356cb8d
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-02-11 18:08:06 +0900

    add some github apis

---
+++ githubSearchCLI/GitHubAPIError.swift
@@ -0,0 +1,16 @@
+struct GitHubAPIError : JSONDecodable, Error {
+    let message: String
+    
+    init(json: Any) throws {
+        guard let dictionary = json as? [String : Any] else {
+            throw JSONDecodeError.invalidFormat(json: json)
+        }
+        
+        guard let message = dictionary["message"] as? String else {
+            throw JSONDecodeError.missingValue(key: "message",
+                                               actualValue: dictionary["message"])
+        }
+        
+        self.message = message
+    }
+}
